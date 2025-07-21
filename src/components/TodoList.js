e88b3fe77b7793b5a9a05d37e2e71ccd0c8090ca
BharatTate <hibharatj@gmail.com>
2025-07-21 14:12:00 +0530

    return added

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,23 +1,20 @@
 import React from 'react'
 
 const TodoList = ({todos, handleComplete}) => {
-
   return (
     <div>
-      <h2>Child Component</h2>
       <ul>
+        <h2>Child Component</h2>
             {
-                todos.map(todo => (
-                <li key={todo.text}>
-                    <p>{todo.text}</p>
-                    {
-                        !todo.completed && (
-                            <button onClick={() => handleComplete(todo.text)}>Complete</button>
-                        )
-                    }
-                </li>
+              todos.map(each => {
+                return (
+                  <li key={each.text}>
+                    <p>{each.text}</p>
+                    {!each.completed && <button onClick={() => handleComplete(each.text)}>Completed</button>}
 
-                ))
+                  </li>
+                )
+              })
             }
       </ul>
     </div>
@@ -25,3 +22,21 @@ const TodoList = ({todos, handleComplete}) => {
 }
 
 export default TodoList
+
+
+
+
+
+// {
+// todos.map(todo => (
+// <li key={todo.text}>
+// <p>{todo.text}</p>
+// {
+// !todo.completed && (
+//   <button onClick={() => handleComplete(todo.text)}>Complete</button>
+// )
+// }
+// </li>
+
+// ))
+// }
