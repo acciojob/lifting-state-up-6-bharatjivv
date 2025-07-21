6993c3d359dc7fac330b80a5f686d802b8c0a277
BharatTate <hibharatj@gmail.com>
2025-07-21 13:11:09 +0530

    Added Parent and Child.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,30 @@
+
 import React, { useState } from "react";
 import './../styles/App.css';
 import TodoList from "./TodoList";
+import Parent from "./Parent";
 
 const App = () => {
-  const [todos, setTodos] = useState([
-    { id: 1, text: 'Learn React', completed: false },
-    { id: 2, text: 'Learn HTML', completed: false },
-    { id: 3, text: 'Learn Javascript', completed: false }
-  ]);
+  // const [todos, setTodos] = useState([
+  //   { id: 1, text: 'Learn React', completed: false },
+  //   { id: 2, text: 'Learn HTML', completed: false },
+  //   { id: 3, text: 'Learn Javascript', completed: false }
+  // ]);
 
-  const handleComplete = (id) => {
-    
-    setTodos((prevTodos) => 
-      prevTodos.map(todo => todo.id === id ? {...todo, completed: true } : todo)
-    );
-  };
+  // const handleComplete = (id) => {
+  //   setTodos((prevTodos) => {
+  //     const updated = prevTodos.map(todo => todo.id === id ? {...todo, completed: true } : todo);
+  //     console.log("updated todos : ", updated)
+  //     return updated;
+  //   });
+  // };
   
   return (
     <div>
         {/* Do not remove the main div */}
-        <h1>Parent component</h1>
-        <TodoList todos = {todos} handleComplete = {handleComplete}/>
+        {/* <h1>Parent component</h1>
+        <TodoList todos = {todos} handleComplete = {handleComplete}/> */}
+        <Parent/>
     </div>
   )
 }
